b52852f3b5dcda3c092f53194b7bc2f90d15cc24
Okazaki Kohei <marie673g@gmail.com>
2022-01-21 20:37:23 +0900

    update

--- tc/110Mbps_10ms.sh
+++ tc/110Mbps_10ms.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
 # shellcheck disable=SC2154
-sudo tc qdisc add dev enp0s9 root handle 1:0 tbf rate 110mbit burst 50kb limit 100kb
-sudo tc qdisc add dev enp0s9 parent 1:1 handle 10:1 netem delay 10ms 0ms distribution normal
+sudo tc qdisc del dev enp0s9 root
+sudo tc qdisc add dev enp0s9 root handle 1:0 tbf rate 100mbit burst 50kb limit 100kb
+sudo tc qdisc add dev enp0s9 parent 1:1 handle 10:1 netem delay 10ms
+
+sudo tc qdisc del dev enp0s10 root
+sudo tc qdisc add dev enp0s10 root handle 1:0 tbf rate 100mbit burst 50kb limit 100kb
+sudo tc qdisc add dev enp0s10 parent 1:1 handle 10:1 netem delay 10ms
