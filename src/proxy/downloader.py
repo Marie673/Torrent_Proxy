84de0bfafa939b323ae78ee35d8346b2b742bf6b
Okazaki Kohei <marie673g@gmail.com>
2022-01-21 16:22:34 +0900

    update

--- src/proxy/downloader.py
+++ src/proxy/downloader.py
@@ -81,6 +81,15 @@ class Run(Process):
 
             time.sleep(0.1)
 
+        last_piece_index = self.pieces_manager.number_of_pieces - 1
+        bitfield = self.m_list[BITFIELD]
+        if not bitfield[last_piece_index]:
+            bitfield[last_piece_index] = True
+            self.m_list[BITFIELD] = bitfield
+            pieces = self.m_list[PIECES]
+            pieces[last_piece_index] = self.pieces_manager.pieces[last_piece_index].raw_data
+            self.m_list[PIECES] = pieces
+
         logging.info("File(s) downloaded successfully.")
         self.display_progression()
         time.sleep(120)
