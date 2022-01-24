e1f210e20b347d7f3656019d08f6123f4f7eef02
Okazaki Kohei <marie673g@gmail.com>
2022-01-24 19:37:08 +0900

    change cefore bundle

--- test/test_code.py
+++ test/test_code.py
@@ -27,6 +27,7 @@ def main():
         exit(1)
 
     data_size = 0
+    bitfield = []
     with cefpyco.create_handle() as h:
         h.send_interest(name, 0)
         start_time = time.time()
@@ -36,12 +37,20 @@ def main():
                 continue
 
             if info.is_data:
-                data_size += len(info.payload)
-                get_data(info)
-                if info.chunk_num == 1:
+                if bitfield[info.chunk_num] is True:
+                    continue
+
+                if info.chunk_num == 0:
+                    bitfield = [False for _ in range(info.end_chunk_num)]
+                    bitfield[0] = True
+                    data_size += len(info.payload)
                     for chunk_num in range(1, info.end_chunk_num):
                         for _ in range(3):
                             h.send_interest(name, chunk_num)
+
+                bitfield[info.chunk_num] = True
+                data_size += len(info.payload)
+
                 if data_size == full_data_size:
                     end_time = time.time() - start_time
                     break
