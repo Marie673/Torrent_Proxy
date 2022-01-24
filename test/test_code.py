9c1c62899cf1224e89e80ff79521e9a062310ed5
Okazaki Kohei <marie673g@gmail.com>
2022-01-24 19:31:24 +0900

    change cefore bundle

--- test/test_code.py
+++ test/test_code.py
@@ -12,14 +12,18 @@ def get_data(info):
 
 def main():
     args = sys.argv
+    name: str = ""
+    full_data_size: int = 0
     if args[1] == '0':
         name = NAME0
+        full_data_size = 1024 * 1024
     elif args[1] == '1':
         name = NAME1
+        full_data_size = 1024 * 1024 * 10
     elif args[1] == '2':
         name = NAME2
+        full_data_size = 1024 * 1024 * 100
     else:
-        name = None
         exit(1)
 
     data_size = 0
@@ -38,7 +42,7 @@ def main():
                     for chunk_num in range(1, info.end_chunk_num):
                         for _ in range(3):
                             h.send_interest(name, chunk_num)
-                else:
+                if data_size == full_data_size:
                     end_time = time.time() - start_time
                     break
 
