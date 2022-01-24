d51564088aed7fd2888456470dd3586d729d5647
Okazaki Kohei <marie673g@gmail.com>
2022-01-24 10:19:06 +0900

    update

--- test/test.py
+++ test/test.py
@@ -1,17 +1,21 @@
+import os
+
 import cefpyco
 
 SIZE=4096
 
-def send_data(h, name, payload):
+def send_file(h, info, file_name):
     cache_time = 360000  # 1時間
-    chunk_num = 0
-    end_chunk_num = len(payload) // SIZE -1
-    while payload:
-        chunk = payload[:SIZE]
-        h.send_data(name=name, payload=chunk,
-                                  chunk_num=chunk_num, end_chunk_num=end_chunk_num, cache_time=cache_time)
-        payload = payload[SIZE:]
-        chunk_num += 1
+    file_size = os.path.getsize(file_name)
+    end_chunk_num = file_size // SIZE
+    chunk = info.chunk_num
+    seek = chunk * SIZE
+    name = info.name
+    with open(file_name, "rb") as file:
+        file.seek(seek)
+        payload = file.read(SIZE)
+        h.send_data(name=name, payload=payload,
+                    chunk_num=chunk, end_chunk_num=end_chunk_num, cache_time=cache_time)
 
 def main():
     with cefpyco.create_handle() as h:
@@ -25,10 +29,7 @@ def main():
             if info.is_interest:
                 print('get interest')
                 if name[2] == '1M.dummy' or name[2] == '10M.dummy' or name[2] == '100M.dummy':
-                    with open(name[2], "rb") as file:
-                        payload = file.read()
-                        file.close()
-                        send_data(h, info.name, payload)
+                    send_file(h, info, name[2])
 
 
 if __name__ == '__main__':
