c8bb71af3649c2e59d57e1d67ead1cc7060c15bf
Okazaki Kohei <marie673g@gmail.com>
2022-01-25 20:44:06 +0900

    idk

--- src/lib/peer.py
+++ src/lib/peer.py
@@ -10,7 +10,6 @@ import message
 
 class Peer(object):
     def __init__(self, number_of_pieces, ip, port=6881):
-        self.time = time.time()
         self.last_call = 0.0
         self.has_handshacked = False
         self.healthy = False
