f066881867a5518b6208f5bd3041fde75fa1850a
Okazaki Kohei <marie673g@gmail.com>
2022-01-26 01:22:30 +0900

    idk

--- test/test_code.py
+++ test/test_code.py
@@ -91,7 +91,7 @@ class Cefore(object):
                 interest.send_interest(self.handle)
                 self.interests[chunk_num] = interest
 
-            t
+
 
         self.active_state = False
         end_time = time.time() - start_time
