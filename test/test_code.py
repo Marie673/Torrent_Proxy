a9aebd173b3e0f0ceb301465d6eabedbc3bc486f
Okazaki Kohei <marie673g@gmail.com>
2022-01-26 00:54:13 +0900

    idk

--- test/test_code.py
+++ test/test_code.py
@@ -57,14 +57,13 @@ class Cefore(object):
                     event.set()
                     continue
 
+                if info.chunk_num in self.interests:
+                    del self.interests[info.chunk_num]
+
                 if self.bitfield[info.chunk_num] is True:
-                    if info.chunk_num in self.interests:
-                        del self.interests[info.chunk_num]
                     continue
 
                 self.bitfield[info.chunk_num] = True
-                if info.chunk_num in self.interests:
-                    del self.interests[info.chunk_num]
                 self.data_size += len(info.payload)
 
             # self.display_progress()
