8ebc835d6dfaf89fd14a7d3d9a5af9db58b82419
Okazaki Kohei <marie673g@gmail.com>
2022-01-25 20:42:45 +0900

    idk

--- src/lib/peer.py
+++ src/lib/peer.py
@@ -53,7 +53,7 @@ class Peer(object):
 
     def is_eligible(self):
         now = time.time()
-        return (now - self.last_call) > 0.2
+        return (now - self.last_call) > 0.001
 
     def has_piece(self, index):
         return self.bit_field[index]
