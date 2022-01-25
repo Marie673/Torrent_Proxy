9eee2c10909ac20109ae78fe6b60e59f6e7e905a
Okazaki Kohei <marie673g@gmail.com>
2022-01-26 00:02:35 +0900

    idk

--- test/test_code.py
+++ test/test_code.py
@@ -11,6 +11,15 @@ NAME2='ccnx:/test/100M.dummy'
 
 event = Event()
 
+class Interest(object):
+    def __init__(self, interest):
+        self.interest = interest
+        self.time = None
+
+    def send_interest(self, handle):
+        self.time = time.time()
+
+
 class Cefore(object):
     def __init__(self, name):
         self.name = name
@@ -19,7 +28,7 @@ class Cefore(object):
         self.bitfield = []
         self.data_size = 0
         self.active_state = True
-
+        self.interests = {}
         self.t_listener = Thread(target=self.listener)
 
         self.t_listener.start()
@@ -45,6 +54,7 @@ class Cefore(object):
                     continue
 
                 if self.bitfield[info.chunk_num] is True:
+                    del self.interests[info.chunk_num]
                     continue
 
                 self.bitfield[info.chunk_num] = True
@@ -57,17 +67,23 @@ class Cefore(object):
         self.handle.send_interest(self.name, 0)
         event.wait()
         print("get first chunk")
+
         while False in self.bitfield:
-            send_interest_num = 0
+            for i in self.interests.values():
+                if time.time() - i.time > 5:
+                    i.time = time.time()
+                    i.send_interest(self.handle)
+
+            if len(self.interests) >= 30:
+                continue
+
             for chunk_num in range(len(self.bitfield)):
                 if self.bitfield[chunk_num]:
                     continue
-                else:
-                    send_interest_num += 1
-                    self.handle.send_interest(self.name, chunk_num)
-                    if send_interest_num >= 32:
-                        send_interest_num = 0
-                        continue
+
+                interest = Interest(self.name + str(chunk_num))
+                interest.send_interest(self.handle)
+                self.interests[chunk_num] = interest
 
         self.active_state = False
         end_time = time.time() - start_time
