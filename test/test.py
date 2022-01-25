7cd1b0d48a549af450b5039d6a482bf571d6eb25
Okazaki Kohei <marie673g@gmail.com>
2022-01-26 02:14:19 +0900

    idk

--- test/test.py
+++ test/test.py
@@ -7,7 +7,7 @@ SIZE = 1024 * 4
 def send_file(h, info, file_name):
     cache_time = 360000  # 1時間
     file_size = os.path.getsize(file_name)
-    end_chunk_num = file_size // SIZE
+    end_chunk_num = file_size // SIZE - 1
     chunk = info.chunk_num
     seek = chunk * SIZE
     name = info.name
