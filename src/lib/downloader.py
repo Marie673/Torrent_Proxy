0ed05dafdb0388452456079241022d7ebba7a56b
Okazaki Kohei <marie673g@gmail.com>
2022-01-25 21:48:03 +0900

    idk

--- src/lib/downloader.py
+++ src/lib/downloader.py
@@ -65,7 +65,7 @@ class Run(object):
                 self.display_progression()
                 prog_time = time.time()
 
-            time.sleep(0)
+            time.sleep(0.01)
 
         logging.info("File(s) downloaded successfully.")
         self.display_progression()
