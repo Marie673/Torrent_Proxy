b767e954242a0ac624e0aff6adf7651d276ae4ba
Okazaki Kohei <marie673g@gmail.com>
2022-01-25 20:39:05 +0900

    idk

--- src/lib/downloader.py
+++ src/lib/downloader.py
@@ -59,7 +59,7 @@ class Run(object):
                 piece_index, block_offset, block_length = data
                 piece_data = message.Request(piece_index, block_offset, block_length).to_bytes()
                 peer.send_to_peer(piece_data)
-                time.sleep(0.001)
+                time.sleep(0.0001)
 
             self.display_progression()
 
