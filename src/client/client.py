b84499d74163fd8b1fe16a528e2a8b6f777b1d84
Okazaki Kohei <marie673g@gmail.com>
2022-01-28 01:02:13 +0900

    idk

--- src/client/client.py
+++ src/client/client.py
@@ -51,7 +51,6 @@ class Run(object):
                 app = cefapp.CefAppConsumer(self.handle)
                 state = app.run(interest)
                 self.display_progression()
-                print("{} {}".format(index,self.pieces_manager.pieces[index].is_full))
                 """
                 if not self.req_piece_flg[index]:
                     interest = '/'.join([PROTOCOL, self.info_hash, str(index)])
