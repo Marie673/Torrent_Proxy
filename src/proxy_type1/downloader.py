8811ecc02edd593b71b45ac7d0392f7839172431
Okazaki Kohei <marie673g@gmail.com>
2022-01-30 07:52:33 +0900

    Request pieces for multiprocessing

--- src/proxy_type1/downloader.py
+++ src/proxy_type1/downloader.py
@@ -47,7 +47,7 @@ class Run(Process):
                     self.request.append(request_index)
 
             for index in self.request:
-
+                print(self.request)
                 if self.pieces_manager.pieces[index].is_full:
                     piece = self.pieces_manager.pieces[index]
                     raw_data = piece.raw_data
