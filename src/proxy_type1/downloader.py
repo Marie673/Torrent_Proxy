43550ca46d7e7080df92f84fc8afffa909f0d75f
Okazaki Kohei <marie673g@gmail.com>
2022-01-30 09:21:14 +0900

    Request pieces for multiprocessing

--- src/proxy_type1/downloader.py
+++ src/proxy_type1/downloader.py
@@ -73,7 +73,7 @@ class Run(Process):
                 piece_data = message.Request(piece_index, block_offset, block_length).to_bytes()
                 peer.send_to_peer(piece_data)
 
-            time.sleep(0.5)
+            time.sleep(1)
 
     def display_progression(self):
         new_progression = 0
