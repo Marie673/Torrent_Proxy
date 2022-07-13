c2405b61cb733ef76af591923befd19330c68898
Okazaki Kohei <marie673g@gmail.com>
2022-07-14 06:19:48 +0900

    fix peers manager

--- src/lib/pieces_manager.py
+++ src/lib/pieces_manager.py
@@ -24,8 +24,7 @@ class PiecesManager(object):
         length = self.bitfield.len
         if not length:
             return ''
-        if length > 250 * 15:
-            return ''.join((self.bitfield._readhex(250 * 15, 0), '...'))
+
         return ''.join((self.bitfield._readhex(length, 0), '...'))
 
     def update_bitfield(self, piece_index):
