8df9af40f42c55650b7d2e42d6ba4140f062f0b9
Okazaki Kohei <marie673g@gmail.com>
2022-07-18 03:43:22 +0900

    fix peers manager

--- src/client/cefapp.py
+++ src/client/cefapp.py
@@ -117,7 +117,7 @@ class CefAppConsumer:
         self.data_size = 0
 
     def run(self):
-        # self.proxy_bitfield.start()
+        self.proxy_bitfield.start()
         self.on_start()
 
         start_time = prog_time = time.time()
@@ -156,6 +156,10 @@ class CefAppConsumer:
                 break
 
             if piece.is_full:
+                continue
+
+            index = piece.piece_index
+            if self.proxy_bitfield.bitfield[index] == 0:
                 continue
 
             name = '/'.join([PROTOCOL, self.info_hash, 'request', str(piece.piece_index)])
