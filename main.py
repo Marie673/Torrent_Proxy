af368a6fa423a2c2197d0598f4498b8f09fb0b8b
Okazaki Kohei <marie673g@gmail.com>
2022-12-09 14:27:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ def main():
     interest_listener.start()
 
     paths = [
-        '/bittorrent/evaluation/torrent/128MB.torrent',
-        '/bittorrent//evaluation/torrent/256MB.torrent',
-        '/bittorrent//evaluation/torrent/512MB.torrent',
-        '/bittorrent//evaluation/torrent/1024MB.torrent',
-        '/bittorrent//evaluation/torrent/2048MB.torrent'
+        '/bittorrent/evaluation/torrent_file/128MB.torrent',
+        '/bittorrent//evaluation/torrent_file/256MB.torrent',
+        '/bittorrent//evaluation/torrent_file/512MB.torrent',
+        '/bittorrent//evaluation/torrent_file/1024MB.torrent',
+        '/bittorrent//evaluation/torrent_file/2048MB.torrent'
     ]
 
     path_dict = {}
