f68e236d6552d6fc534f0286728e57b80dcfec15
Okazaki Kohei <marie673g@gmail.com>
2022-12-11 21:23:50 +0900

    fix:

--- src/domain/entity/piece/piece.py
+++ src/domain/entity/piece/piece.py
@@ -48,6 +48,8 @@ class Piece(object):
         if not self.is_full and not self.blocks[index].state == State.FULL:
             self.blocks[index].data = data
             self.blocks[index].state = State.FULL
+        else:
+            logger.error("invalid block")
 
     def get_block(self, block_offset, block_length):
         if self.exist:
