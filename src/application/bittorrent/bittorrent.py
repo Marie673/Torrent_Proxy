ac1158455028f8ac3bc843dd699b775a27b1edf0
Okazaki Kohei <marie673g@gmail.com>
2022-12-11 21:50:44 +0900

    fix:

--- src/application/bittorrent/bittorrent.py
+++ src/application/bittorrent/bittorrent.py
@@ -81,7 +81,7 @@ class BitTorrent(Thread):
                     if piece.is_full:
                         continue
                     self.request_piece(index)
-                    time.sleep(0.01)
+                    time.sleep(0.05)
 
             self._update_bitfield_file()
         finally:
