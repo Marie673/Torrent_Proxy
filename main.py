c9f8c3c208bcc6558ead7a045c16c2dcc03db0dc
Okazaki <net.kohei.okazaki@gmail.com>
2023-07-24 04:53:32 +0900

    change

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 import subprocess
 
-from src.global_value import com_manager
-import src.global_value as gv
 from src.application.interest_listener import InterestListener
-from src.application.bittorrent.communication_manager import CommunicationManager
+
 
 def main():
     res = subprocess.run(["cefnetdstart"], shell=True)
 
-    com_manager = CommunicationManager()
     c_process = InterestListener()
 
     c_process.start()
