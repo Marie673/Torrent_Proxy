60c3cea87134e10fdc3a5e2a171e0cecf915c488
Okazaki <net.kohei.okazaki@gmail.com>
2023-07-27 07:43:22 +0900

    change

--- src/application/interest_listener.py
+++ src/application/interest_listener.py
@@ -26,7 +26,7 @@ class InterestListener:
             while True:
                 try:
                     info = self.cef_handle.receive()
-                    logger.debug(info)
+                    # logger.debug(info)
                     if info.is_succeeded and info.is_interest :
                         await self.handle_interest(info)
                 except Exception as e:
